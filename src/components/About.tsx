46db35a7111f03c21e1b76111fc04c59a82a9d5c
Benyamin Simanjuntak <benyamin.s.o.simanjuntak@amsconsult.com>
2025-05-19 15:34:37 +0700

    Fix about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,7 @@ const About: React.FC = () => {
             <h2 className="text-3xl font-bold text-portfolio-dark mb-6">
               Let’s turn ideas into real, user-friendly products.
             </h2>
-            <p className="text-lg md:text-xl mb-10 max-w-3xl leading-relaxed">
+            <p className="text-lg md:text-xl mb-10 leading-relaxed">
              I’m a frontend developer with over 8 years of experience crafting responsive websites and mobile apps. I specialize in ReactJS and VueJS for web, and also develop Android apps with Java and Kotlin.
             I focus on creating apps that are not only fast and functional but also easy and enjoyable to use. I believe great products come from a mix of good technology, thoughtful design, and strong teamwork.
             Whether you’re building something from scratch or improving an existing product, I’m excited to help make it happen.
@@ -25,16 +25,16 @@ const About: React.FC = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
         <div>
-          <h3 className="text-2xl font-semibold">Front-End</h3>
-          <p className="text-sm mt-2 text-gray-400">60+ Hours Experience</p>
+          <h3 className="text-2xl font-semibold">Software Development</h3>
+          <p className="text-sm mt-2 text-gray-400">8+ Years Experience</p>
         </div>
         <div>
-          <h3 className="text-2xl font-semibold">Python</h3>
-          <p className="text-sm mt-2 text-gray-400">4 Years Experience</p>
+          <h3 className="text-2xl font-semibold">Front End Developer</h3>
+          <p className="text-sm mt-2 text-gray-400">3+ Years Experience</p>
         </div>
         <div>
-          <h3 className="text-2xl font-semibold">Machine Learning</h3>
-          <p className="text-sm mt-2 text-gray-400">120+ Hours Experience</p>
+          <h3 className="text-2xl font-semibold">Android Developer</h3>
+          <p className="text-sm mt-2 text-gray-400">5+ Years Experience</p>
         </div>
       </div>
           </div>
